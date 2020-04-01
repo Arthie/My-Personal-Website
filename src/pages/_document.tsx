1ca4f2c4e101957a87b5d212700c55f9dd9e2bf1
Arthur Petrie <contact@arthurpetrie.com>
2020-04-01 23:55:39 +0200

    refactor + add font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -20,6 +20,7 @@ class MyDocument extends Document {
           />
           <meta name="Description" content="Personal website Arthur Petrie" />
           <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>⚡</text></svg>" />
+          <link href="/fonts/Inter/inter.css" rel="stylesheet" />
         </Head>
         <body>
           <Main />
