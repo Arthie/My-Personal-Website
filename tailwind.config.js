b0a91f0891561099056156169f39491f55434cf6
Arthur Petrie <contact@arthurpetrie.com>
2020-08-28 08:57:05 +0200

    fix dev mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,14 +10,9 @@ module.exports = {
       fontSize: {
         "7xl": "5rem",
       },
-      boxShadow: {
-        "neumorphism-blue": "20px 20px 60px #1e3064, -20px -20px 60px #284088",
-        "neumorphism-blue-inset":
-          "inset 20px 20px 60px #1e3064, inset -20px -20px 60px #284088",
-      },
-      gridTemplateRows: {
-        header: "auto 1fr",
-      },
+      // gridTemplateRows: {
+      //   header: "auto 1fr",
+      // },
     },
   },
   variants: {},
