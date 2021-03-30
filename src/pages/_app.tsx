9e85f098d5b7f50353ebe7aca16fb6df748dfaf3
Arthur Petrie <contact@arthurpetrie.com>
2021-03-31 01:06:08 +0200

    add crossOrigin

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -27,7 +27,11 @@ function App({ Component, pageProps, router }: AppProps) {
           rel="icon"
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>⚡</text></svg>"
         />
-        <link rel="preconnect" href="https://www.google-analytics.com" />
+        <link
+          rel="preconnect"
+          href="https://www.google-analytics.com"
+          crossOrigin="anonymous"
+        />
         <style
           dangerouslySetInnerHTML={{
             __html: `
